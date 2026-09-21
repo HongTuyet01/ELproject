0ad433716b9b2bf292040432edd01951986399ca
nguyen thi hong tuyet <22521637@gm.uit.edu.vn>
2026-09-21 15:01:36 +0700

    Refactor Puppeteer test to log HTML content from root and dashboard pages

--- test-puppeteer.js
+++ test-puppeteer.js
@@ -2,19 +2,12 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  await page.goto('https://e-lproject-l5tv.vercel.app/', { waitUntil: 'networkidle0' });
+  const html = await page.evaluate(() => document.body.innerHTML);
+  console.log('ROOT HTML:', html.substring(0, 300));
   
   await page.goto('https://e-lproject-l5tv.vercel.app/dashboard', { waitUntil: 'networkidle0' });
-  console.log('Navigated to dashboard');
-  
-  // Click on the garden card
-  await page.click('h3.line-clamp-2');
-  console.log('Clicked garden card');
-  
-  // Wait a bit for animation
-  await new Promise(r => setTimeout(r, 1000));
-  
-  const html = await page.evaluate(() => document.querySelector('main').innerHTML);
-  console.log('MAIN HTML AFTER CLICK:', html.substring(0, 500));
+  const dashboardHtml = await page.evaluate(() => document.body.innerHTML);
+  console.log('DASHBOARD HTML:', dashboardHtml.substring(0, 300));
   await browser.close();
 })();
