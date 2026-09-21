2823f56bc80146cca0472a51b02e83ee0b5e2b76
nguyen thi hong tuyet <22521637@gm.uit.edu.vn>
2026-09-21 15:05:27 +0700

    Remove framer-motion from Layout completely to fix Safari bug

--- agricarbon-react/src/App.jsx
+++ agricarbon-react/src/App.jsx
@@ -46,18 +46,7 @@ const Layout = ({ children }) => {
       )}
       
       <main className={`flex-grow ${isIntro ? '' : 'container mx-auto p-4 md:p-8'}`}>
-        <> 
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.3 }}
-            className="h-full"
-          >
-            {children}
-          </motion.div>
-        </>
+        {children}
       </main>
 
       {!isIntro && (
